9f12818390b2dec8bde825de783411cbaec1bec3
pyxudev <xsq51py@gmail.com>
2026-03-19 13:47:49 +0900

    Allow fetch api

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://ai-auto-sns.lowlevelengineers.com, http://localhost:3000, http://127.0.0.1:3000",
-          },
+            value: "*"
+            },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,POST",
